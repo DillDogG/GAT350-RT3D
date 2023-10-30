af0523636d46bf76c5e5a8d3d12cb0e62c8efeca
DillDogG <dilldoggaming@gmail.com>
2023-10-30 12:07:28 -0600

    Updated Lighting

---
+++ Build/Assets/Shaders/lit_phong.vert
@@ -0,0 +1,38 @@
+#version 430
+
+in layout(location = 0) vec3 vposition;
+in layout(location = 1) vec3 vnormal;
+in layout(location = 2) vec2 vtexcoord;
+
+out layout(location = 0) vec3 oposition;
+out layout(location = 1) vec3 onormal;
+out layout(location = 2) vec2 otexcoord;
+
+uniform mat4 model;
+uniform mat4 projection;
+uniform mat4 view;
+
+uniform struct Material
+{
+	uint params;
+	vec3 albedo;
+	vec3 specular;
+	vec3 emissive;
+	float shininess;
+
+	vec2 offset;
+	vec2 tiling;
+} material;
+
+void main()
+{
+	mat4 modelView = view * model;
+	oposition = vec3(model * vec4(vposition, 1));
+	//onormal = mat3(transpose(inverse(model))) * vnormal;
+	onormal = normalize(mat3(modelView) * vnormal);
+	otexcoord = (vtexcoord * material.tiling) + material.offset;
+
+
+	mat4 mvp = projection * view * model;
+	gl_Position = mvp * vec4(vposition, 1.0);
+}
--- Source/Engine/Renderer/Material.h
+++ Source/Engine/Renderer/Material.h
@@ -5,13 +5,16 @@
 #include <vector>
 #include <memory>
 
-namespace nc
-{
+namespace nc {
 	class Texture;
 	class Program;
 
-	class Material : public Resource
-	{
+	class Material : public Resource {
+	public:
+		const uint32_t ALBEDO_TEXTURE_MASK   = (1 << 0); // 0001
+		const uint32_t SPECULAR_TEXTURE_MASK = (1 << 1); // 0010
+		const uint32_t NORMAL_TEXTURE_MASK   = (1 << 2); // 0100
+		const uint32_t EMISSIVE_TEXTURE_MASK = (1 << 3); // 1000
 	public:
 		virtual bool Create(std::string filename, ...) override;
 
@@ -22,11 +25,18 @@ namespace nc
 		void ProcessGui();
 
 	public:
-		glm::vec3 diffuse{ 1 };
+		uint32_t params{ 0 };
+		glm::vec3 albedo{ 1 };
 		glm::vec3 specular{ 1 };
+		glm::vec3 emissive{ 0 };
 		float shininess = 2;
 		glm::vec2 tiling{ 1, 1 };
 		glm::vec2 offset{ 0, 0 };
+
+		res_t<Texture> albedoTexture;
+		res_t<Texture> specularTexture;
+		res_t<Texture> normalTexture;
+		res_t<Texture> emissiveTexture;
 		//glm::vec3 ambientLight{ 0.2f, 0.2f, 0.2f };
 		//glm::vec3 diffuseLight{ 1, 1, 1 };
 		//glm::vec3 lightPosition{ 0, 8, 0 };

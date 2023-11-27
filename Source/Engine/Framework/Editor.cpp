dc4fabf9728f11e73332260e784f697c32493586
DillDogG <dilldoggaming@gmail.com>
2023-11-27 11:12:56 -0700

    Filtered Resources

--- Source/Engine/Framework/Editor.cpp
+++ Source/Engine/Framework/Editor.cpp
@@ -9,14 +9,43 @@ namespace nc {
 
 	void nc::Editor::ProcessGui(Scene* scene) {
 		if (!m_active) return;
+		//ImGui::Begin("Resources");
+		//auto resources = GET_RESOURCES(Resource);
+		//for (auto& resource : resources) {
+		//	if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) {
+		//		m_selected = resource.get();
+		//	}
+		//}
+		// show resources
 		ImGui::Begin("Resources");
-		auto resources = GET_RESOURCES(Resource);
-		for (auto& resource : resources) {
-			if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) {
-				m_selected = resource.get();
+		// materials
+		if (ImGui::CollapsingHeader("Materials")) {
+			auto resources = GET_RESOURCES(Material);
+			for (auto& resource : resources) {
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
 			}
 		}
-
+		// models
+		if (ImGui::CollapsingHeader("Models")) {
+			auto resources = GET_RESOURCES(Model);
+			for (auto& resource : resources) {
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
+			}
+		}
+		// shaders
+		if (ImGui::CollapsingHeader("Shaders")) {
+			auto resources = GET_RESOURCES(Shader);
+			for (auto& resource : resources) {
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
+			}
+		}
+		// textures
+		if (ImGui::CollapsingHeader("Textures")) {
+			auto resources = GET_RESOURCES(Texture);
+			for (auto& resource : resources) {
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
+			}
+		}
 		ImGui::End();
 
 		// show scene
